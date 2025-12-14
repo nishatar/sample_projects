cbcb8c39a9777f02805ed77b7ca8aa7619141061
nishatar <nishaabc282@gmail.com>
2025-12-15 02:33:00 +0500

    Add files via upload

---
+++ java.js
@@ -0,0 +1,49 @@
+let name = "nisha"
+let age = 22;
+name:console.log (name);
+age:console.log(age);
+/* sum of two numbers8*/
+let a = 3;
+let b = 4;
+let add = a+b;
+let sub = a-b;
+console.log ( "sum is:" + add) ;
+console.log ( "sum is:" + sub) ;
+/* array practice*/
+let  names = ["nisha" , "ayesha", "komal", "zohaib" ];
+console.log( "first name is" , names[0]);
+console.log( "first name is" , names[1]);
+console.log( "first name is" , names[2]);
+console.log(  "lenght of list" ,  names.length);
+console.log( "add new name" , names.unshift("aliha"));
+console.log( "first name is" , names.push("durein"));
+names.unshift("aliha");
+console.log("After unshift:", names);
+
+names.push("durein");
+console.log("After push:", names);
+/* function*/
+function fruits(fruits) {
+   console.log("Fruit is: " + fruits);
+}
+
+fruits("mango");
+fruits("orange");
+let i = 6;
+while (i <= 5) {
+  console.log(i);
+  i++;
+  if(i===6){
+    console.log("condition is true" + i);
+  }
+    else {
+         console.log("condition is false" + i);
+    }
+  }
+
+
+
+
+
+
+
